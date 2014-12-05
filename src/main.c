70427e110a6eade12b2ffbffdf9526d14031200e
robisodd <robisodd@gmail.com>
2014-12-04 20:41:24 -0500

    Drawing function separated

--- src/main.c
+++ src/main.c
@@ -63,17 +63,7 @@ void GenerateMap(int32_t startx, int32_t starty) {
     }
   } //End While True
 }
-/*
-int8_t getxmap(int16_t x, int16_t y) {
-  if (x<0 || x>=MAPSIZE || y<0 || y>=MAPSIZE) return -1;
-  return map[(int)y * MAPSIZE + (int)x];
-}
 
-void setxmap(int16_t x, int16_t y, int8_t value) {
-  if ((x >= 0) && (x < MAPSIZE) && (y >= 0) && (y < MAPSIZE))
-    map[y * MAPSIZE + x] = value;
-}
-*/
 int8_t getmap(int32_t x, int32_t y) {
   x=x>>6; y=y>>6;
   if (x<0 || x>=mapsize || y<0 || y>=mapsize) return -1;
@@ -86,20 +76,141 @@ void setmap(int32_t x, int32_t y, int8_t value) {
     map[y * mapsize + x] = value;
 }
 
+int32_t min(int32_t a, int32_t b) {return (a<b)?a:b;}
+int32_t max(int32_t a, int32_t b) {return (a>b)?a:b;}
+
 // ------------------------------------------------------------------------ //
 //  Drawing Functions
 // ------------------------------------------------------------------------ //
+void fill_window(GContext *ctx, uint8_t *data) {
+  for(uint16_t y=0, yaddr=0; y<168; y++, yaddr+=20)
+    for(uint16_t x=0; x<19; x++)
+      ((uint8_t*)(((GBitmap*)ctx)->addr))[yaddr+x] = data[y%8];
+}
+  //uint8_t *ctx8 = ((uint8_t*)(((GBitmap*)ctx)->addr));
+static void draw_map(GContext *ctx, GRect box, int32_t zoom) {
+  //1-pixel-per-square map:
+  //for (int16_t x = 0; x < mapsize; x++) for (int16_t y = 0; y < mapsize; y++) {graphics_context_set_stroke_color(ctx, map[y*mapsize+x]>0?1:0); graphics_draw_pixel(ctx, GPoint(x, y));}
+  uint32_t *ctx32 = ((uint32_t*)(((GBitmap*)ctx)->addr));
+  int32_t x,y, yaddr, xaddr, xbit;
+  int32_t xonmap, yonmap, yonmapinit;
+
+  xonmap = ((player.x*zoom)>>6) - (box.size.w/2);  // Divide by ZOOM to get map X coord, but rounds [-ZOOM to 0] to 0 and plots it, so divide by ZOOM after checking if <0
+  yonmapinit = ((player.y*zoom)>>6) - (box.size.h/2);
+  for(x=0; x<box.size.w; x++, xonmap++) {
+    xaddr = (x+box.origin.x) >> 5;        // X memory address
+    xbit = ~(1<<((x+box.origin.x) & 31)); // X bit shift level (normally wouldn't ~ it, but ~ is used more often than not)
+    if(xonmap>=0 && xonmap<(mapsize*zoom)) {
+      yonmap = yonmapinit;
+      yaddr = box.origin.y * 5;           // Y memory address
+      for(y=0; y<box.size.h; y++, yonmap++, yaddr+=5) {
+        if(yonmap>=0 && yonmap<(mapsize*zoom)) {             // If within Y bounds
+          if(map[(((yonmap/zoom)*mapsize))+(xonmap/zoom)]>0) //   Map shows a wall >0
+            ctx32[xaddr + yaddr] |= ~xbit;                   //     White dot
+          else                                               //   Map shows <= 0
+            ctx32[xaddr + yaddr] &= xbit;                    //     Black dot
+        } else {                                             // Else: Out of Y bounds
+          ctx32[xaddr + yaddr] &= xbit;                      //   Black dot
+        }
+      }
+    } else {                                // Out of X bounds: Black vertical stripe
+      for(yaddr=box.origin.y*5; yaddr<((box.size.h + box.origin.y)*5); yaddr+=5)
+        ctx32[xaddr + yaddr] &= xbit;
+    }
+  }
+
+  graphics_context_set_fill_color(ctx, (time_ms(NULL, NULL) % 250)>125?0:1);                      // Flashing dot
+  graphics_fill_rect(ctx, GRect((box.size.w/2)+box.origin.x - 1, (box.size.h/2)+box.origin.y - 1, 3, 3), 0, GCornerNone); // Square Cursor
+
+  graphics_context_set_stroke_color(ctx, 1); graphics_draw_rect(ctx, GRect(box.origin.x-1, box.origin.y-1, box.size.w+2, box.size.h+2)); // White Border
+}
+
+static void draw_mini_map(GContext *ctx, uint32_t box_x, uint32_t box_y, uint32_t box_w, uint32_t box_h, int32_t zoom) {
+  //1-pixel-per-square map:
+  //for (int16_t x = 0; x < mapsize; x++) for (int16_t y = 0; y < mapsize; y++) {graphics_context_set_stroke_color(ctx, map[y*mapsize+x]>0?1:0); graphics_draw_pixel(ctx, GPoint(x, y));}
+  uint32_t *ctx32 = ((uint32_t*)(((GBitmap*)ctx)->addr));
+  uint32_t x,y, yaddr, xaddr, xbit;
+  int32_t xonmap, yonmap, yonmapinit;
+
+  xonmap = ((player.x*zoom)>>6) - (box_w/2);  // Divide by ZOOM to get map X coord, but rounds [-ZOOM to 0] to 0 and plots it, so divide by ZOOM after checking if <0
+  yonmapinit = ((player.y*zoom)>>6) - (box_h/2);
+  for(x=0; x<box_w; x++, xonmap++) {
+    xaddr = (x+box_x) >> 5;        // X memory address
+    xbit = ~(1<<((x+box_x) & 31)); // X bit shift level (normally wouldn't ~ it, but ~ is used more often than not)
+    if(xonmap>=0 && xonmap<(mapsize*zoom)) {
+      yonmap = yonmapinit;
+      yaddr = box_y * 5;           // Y memory address
+      for(y=0; y<box_h; y++, yonmap++, yaddr+=5) {
+        if(yonmap>=0 && yonmap<(mapsize*zoom)) {             // If within Y bounds
+          if(map[(((yonmap/zoom)*mapsize))+(xonmap/zoom)]>0) //   Map shows a wall >0
+            ctx32[xaddr + yaddr] |= ~xbit;                   //     White dot
+          else                                               //   Map shows <= 0
+            ctx32[xaddr + yaddr] &= xbit;                    //     Black dot
+        } else {                                             // Else: Out of Y bounds
+          ctx32[xaddr + yaddr] &= xbit;                      //   Black dot
+        }
+      }
+    } else {                                // Out of X bounds: Black vertical stripe
+      for(yaddr=box_y*5; yaddr<((box_h + box_y)*5); yaddr+=5)
+        ctx32[xaddr + yaddr] &= xbit;
+    }
+  }
+
+  graphics_context_set_fill_color(ctx, (time_ms(NULL, NULL) % 250)>125?0:1);                      // Flashing dot
+  graphics_fill_rect(ctx, GRect((box_w/2)+box_x - 1, (box_h/2)+box_y - 1, 3, 3), 0, GCornerNone); // Square Cursor
+
+  graphics_context_set_stroke_color(ctx, 1); graphics_draw_rect(ctx, GRect(box_x-1, box_y-1, box_w+2, box_h+2)); // White Border
+}
+
 static void graphics_layer_update(Layer *me, GContext *ctx) {
-  /*
+
+  //draw_mini_map(ctx, 4, 4, 100, 100, ZOOM);
+  //draw_mini_map(ctx, 4, 110, 40, 40, 4);
+  draw_map(ctx, GRect(4, 4, 100, 100), ZOOM);
+  draw_map(ctx, GRect(4, 110, 40, 40), 4);
+
   static char text[40];  //Buffer to hold text
-  GRect textframe = GRect(0, 123, 143, 20);  // Text Box Position and Size
-  snprintf(text, sizeof(text), " x:%d y:%d", (int)(cursorx), (int)(cursory));  // What text to draw
+  GRect textframe = GRect(24, 0, 100, 20);  // Text Box Position and Size
+  snprintf(text, sizeof(text), "x:%ld y:%ld z:%ld", (player.x), (player.y), ZOOM);  // What text to draw
   graphics_context_set_fill_color(ctx, 0); graphics_fill_rect(ctx, textframe, 0, GCornerNone);  //Black Filled Rectangle
   graphics_context_set_stroke_color(ctx, 1); graphics_draw_rect(ctx, textframe);                //White Rectangle Border
   graphics_context_set_text_color(ctx, 1);  // Text Color
-  graphics_draw_text(ctx, text, fonts_get_system_font(FONT_KEY_GOTHIC_14), textframe, GTextOverflowModeWordWrap, GTextAlignmentLeft, NULL);  //Write Text
-*/
+  graphics_draw_text(ctx, text, fonts_get_system_font(FONT_KEY_GOTHIC_14), textframe, GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);  //Write Text
 
+}
+
+/*
+  //graphics_context_set_stroke_color(ctx, (time_ms(NULL, NULL) % 250)>125?0:1);  // Flashing dot
+  //graphics_draw_pixel(ctx, GPoint((overlay.size.w/2)+overlay.origin.x, (overlay.size.h/2)+overlay.origin.y));
+  for (int16_t x = 0; x < mapsize; x++) for (int16_t y = 0; y < mapsize; y++) {graphics_context_set_stroke_color(ctx,  map[y*mapsize+x]>0?1:0); graphics_draw_pixel(ctx, GPoint(x, y));}
+
+  GRect overlay;
+  overlay = GRect(24,24,100,100);
+  GRect dot;
+  
+  for (int16_t x = 0; x < mapsize; x++)
+    for (int16_t y = 0; y < mapsize; y++) {
+      graphics_context_set_fill_color(ctx,  map[y*mapsize+x]>0?1:0);
+      dot.origin.x = (x*ZOOM) + (overlay.size.w/2) - ((player.x*ZOOM)>>6);
+      dot.origin.y = (y*ZOOM) + (overlay.size.h/2) - ((player.y*ZOOM)>>6);
+      
+      
+      x - (overlay.size.w/2) + ((player.x*ZOOM)>>6);= (xonmap*ZOOM) 
+      
+      dot.size.w = ZOOM;
+      dot.size.h = ZOOM;
+      if(dot.origin.x+ZOOM>=0 && dot.origin.y+ZOOM>=0 && dot.origin.x<overlay.size.w && dot.origin.y<overlay.size.h) {
+        if(dot.origin.x<0) {dot.size.w += dot.origin.x; dot.origin.x=0;}
+        if(dot.origin.y<0) {dot.size.h += dot.origin.y; dot.origin.y=0;}
+        if(dot.origin.x+dot.size.w>overlay.size.w) dot.size.w = (overlay.size.w - dot.origin.x);
+        if(dot.origin.y+dot.size.h>overlay.size.h) dot.size.h = (overlay.size.h - dot.origin.y);
+        
+        dot.origin.x += overlay.origin.x;
+        dot.origin.y += overlay.origin.y;
+        graphics_fill_rect(ctx, dot, 0, GCornerNone);
+     }
+  }
+*/
 /*  
   int16_t MAP_X, MAP_Y;
   MAP_X = 22;
@@ -118,60 +229,6 @@ static void graphics_layer_update(Layer *me, GContext *ctx) {
         //graphics_fill_rect(ctx, GRect((x*ZOOM)+MAP_X+3, (y*ZOOM)+MAP_Y+3, 2, 2), 0, GCornerNone); // Special Dot
       //} 
 */
-  
-//map pos
-//mapsize x, y
-//block size (ZOOM)
-//player pos
-  for (int16_t x = 0; x < mapsize; x++) for (int16_t y = 0; y < mapsize; y++) {graphics_context_set_stroke_color(ctx,  map[y*mapsize+x]>0?1:0); graphics_draw_pixel(ctx, GPoint(x, y));}
-  
-  GRect overlay;
-  overlay.origin.x = 22;
-  overlay.origin.y = 22;
-  overlay.size.w = 100;
-  overlay.size.h = 100;
-  
-  GRect dot;
-  int32_t transx, transy;
-  transx = (player.x*ZOOM)>>6;
-  transy = (player.y*ZOOM)>>6;
-  
-  for (int16_t x = 0; x < mapsize; x++)
-    for (int16_t y = 0; y < mapsize; y++) {
-      graphics_context_set_fill_color(ctx,  map[y*mapsize+x]>0?1:0);
-      dot.origin.x = overlay.origin.x + (x*ZOOM);// + ((ZOOM*mapsize)/2) - transx;
-      dot.origin.y = overlay.origin.y + (y*ZOOM);// + ((ZOOM*mapsize)/2) - transy;
-      dot.size.w = ZOOM;
-      dot.size.h = ZOOM;
-      graphics_fill_rect(ctx, dot, 0, GCornerNone);
-  }
-    //graphics_context_set_stroke_color(ctx, 1);
-    graphics_context_set_stroke_color(ctx, (time_ms(NULL, NULL) % 250)>125?0:1);  // Flashing dot
-    graphics_draw_pixel(ctx, GPoint(transx+overlay.origin.x, transy+overlay.origin.y));
-
-      //if(getxmap(x,y)==0) graphics_context_set_fill_color(ctx, 0); else graphics_context_set_fill_color(ctx, 1); // Black if map cell = 0, else white
-      //graphics_fill_rect(ctx, GRect((x*ZOOM)+MAP_X, (y*ZOOM)+MAP_Y, ZOOM, ZOOM), 0, GCornerNone);
-      //if(getxmap(x,y)==2) {
-        //graphics_context_set_fill_color(ctx, 1);
-        //graphics_draw_pixel(ctx, GPoint((x*ZOOM)+MAP_X+2, (y*ZOOM)+MAP_Y+2)); // Special Dot
-        //graphics_fill_rect(ctx, GRect((x*ZOOM)+MAP_X+3, (y*ZOOM)+MAP_Y+3, 2, 2), 0, GCornerNone); // Special Dot
-      //} 
-
-    
-  //graphics_context_set_fill_color(ctx, (time_ms(NULL, NULL) % 250)>125?0:1);  // Flashing dot
-      //graphics_fill_rect(ctx, GRect((player.x*ZOOM)+MAP_X+1, (player.y*ZOOM)+MAP_Y+1, ZOOM-2, ZOOM-2), 0, GCornerNone); // Full Square Player
-      //graphics_fill_rect(ctx, GRect((cursorx*ZOOM)+MAP_X+1, (cursory*ZOOM)+MAP_Y+1, ZOOM-2, ZOOM-2), 0, GCornerNone); // Smaller Square Cursor
-
-  static char text[40];  //Buffer to hold text
-  GRect textframe = GRect(0, 0, 143, 20);  // Text Box Position and Size
-  snprintf(text, sizeof(text), " x:%ld y:%ld", (player.x), (player.y));  // What text to draw
-  graphics_context_set_fill_color(ctx, 0); graphics_fill_rect(ctx, textframe, 0, GCornerNone);  //Black Filled Rectangle
-  graphics_context_set_stroke_color(ctx, 1); graphics_draw_rect(ctx, textframe);                //White Rectangle Border
-  graphics_context_set_text_color(ctx, 1);  // Text Color
-  graphics_draw_text(ctx, text, fonts_get_system_font(FONT_KEY_GOTHIC_14), textframe, GTextOverflowModeWordWrap, GTextAlignmentLeft, NULL);  //Write Text
- 
-}
-
 // ------------------------------------------------------------------------ //
 //  Timer Functions
 // ------------------------------------------------------------------------ //
@@ -187,8 +244,8 @@ static void timer_callback(void *data) {
   accel_service_peek(&accel);                             // read accelerometer
   //walk(player.facing, accel.y>>4);                        // walk based on accel.y  Technically: walk(accel.y * 64px / 1000);
   //player.facing += (accel.x<<4);                        //   spin
-  int32_t dx = (accel.x>>5);
-  int32_t dy = (accel.y>>5);
+  int32_t dx = (accel.x>>4);
+  int32_t dy = (accel.y>>4);
   if(getmap(player.x + dx, player.y) <= 0 || IDCLIP) player.x += dx;
   if(getmap(player.x, player.y - dy) <= 0 || IDCLIP) player.y -= dy;
   
@@ -203,7 +260,7 @@ void     up_single_click_handler(ClickRecognizerRef recognizer, void *context) {
 void   down_single_click_handler(ClickRecognizerRef recognizer, void *context) {ZOOM++; if(ZOOM>20) ZOOM=20;}
 void select_single_click_handler(ClickRecognizerRef recognizer, void *context) {
   GenerateMap(mapsize/2, 0);
-  player = (PlayerStruct){.x=(64*(mapsize/2)), .y=(2 * 64), .facing=10000};
+  player = (PlayerStruct){.x=(64*(mapsize/2)), .y=(-2 * 64), .facing=10000};
 }
 void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_UP, up_single_click_handler);
